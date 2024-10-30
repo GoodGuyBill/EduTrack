0afd4980ee0ba3e9c3731f99fec334160fc1e66a
GoodGuyBill <wbyrd7@my.wgu.edu>
2024-10-29 20:52:32 -0500

    Add project files.

---
+++ EduTrack/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EduTrack
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
